7aa606fa0b4b4c39f04528b4c6e38db26040cc66
Jessie <jessietajalle08@gmail.com>
2024-08-12 23:41:24 -0700

    Added debugger display to vendor class

--- EntityFrameworkQueries/Models/Vendor.cs
+++ EntityFrameworkQueries/Models/Vendor.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace EntityFrameworkQueries;
 
+[DebuggerDisplay("Name: {VendorName,nq} - {VendorState,nq}")]
 public partial class Vendor
 {
     public int VendorId { get; set; }
